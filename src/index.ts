5f015ad3c154938c4bfe6ec1519f8e1e08689052
ringo360 <ringoxd@ringoxd.dev>
2024-11-17 12:00:50 +0900

    add ffmpeg-static for fix build error

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,10 @@
-console.log("Hello via Bun!");
+import consola from 'consola';
+import { Client } from 'discord.js-selfbot-v13';
+
+const token = await consola.prompt('Input token: ');
+Runner(token as string);
+
+async function Runner(token: string) {
+    const client = new Client();
+    client.login(token);
+}
